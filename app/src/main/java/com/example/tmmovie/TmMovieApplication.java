68a255a7f9802065faedfcdc2fe9b07f898159cb
pm2am <hsrathore.code@gmail.com>
2025-02-06 23:32:09 +0530

    Adding custom Application class for hilt setup

---
+++ app/src/main/java/com/example/tmmovie/TmMovieApplication.java
@@ -0,0 +1,8 @@
+package com.example.tmmovie;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class TmMovieApplication extends Application {}
